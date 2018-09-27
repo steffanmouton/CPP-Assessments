0e67b21e89fd91bfc4ab42e04c591ccbc85be89c
Steffan Mouton <smouton8993@gmail.com>
2018-09-27 12:40:18 -0500

    Added comments to NGG

--- NumberGuessingGame/Source.cpp
+++ NumberGuessingGame/Source.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+	//Seed for Random Number Generation
 	srand(time(NULL));
 	int guess = 0;
 	char input = ' ';
@@ -10,16 +11,18 @@ int main()
 	std::cout << "Think of a number between 1 and 20\n Press Enter to continue. \n";
 	system("pause");
 
-
+	//Initialize Guess min and max
 	int minGuess = 0;
 	int maxGuess = 20;
 	guess = rand() % (maxGuess - minGuess + 1) + minGuess;
 
+	//Run the game
 	while (input != 'E' || 'e')
 	{
 		std::cout << "Is your number '>', '<', or '=' to " << guess << "?\n Input 'E' at any time to exit. \n";
 		std::cin >> input;
 
+		//Switch based on user input. Takes input, adjusts min and max, generates new guess
 		switch (input)
 		{
 		case '>':
